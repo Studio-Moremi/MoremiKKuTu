0a8ada23f8c5a5b50a607c7fc98f8b6b3d44089a
cookedas1 <mulmando1991@gmail.com>
2024-12-14 14:38:17 +0900

    Create main.js

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const recommendationBtn = document.querySelector("#btn-recommendation");
+  if (recommendationBtn) {
+    recommendationBtn.addEventListener("click", () => {
+      alert("공사중");
+    });
+  }
+
+  const downloadBtn = document.querySelector("#btn-download");
+  if (downloadBtn) {
+    downloadBtn.addEventListener("click", () => {
+      alert("공사중");
+    });
+  }
+});
