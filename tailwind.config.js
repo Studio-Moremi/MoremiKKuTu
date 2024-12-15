1760a00f2d3e8a3318f297f42c987b5f55ca4fb8
cookedas1 <mulmando1991@gmail.com>
2024-12-15 11:11:49 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './Server/Web/public/views/**/*.html',
+    './Server/Web/public/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+};
